f010e68186ab94b555a58d736dad5b73996842c7
HoangThienk2 <thienhn2k2@gmail.com>
2025-07-04 09:47:53 +0700

    update feature bar

--- src/components/FeaturesBar.jsx
+++ src/components/FeaturesBar.jsx
@@ -9,14 +9,14 @@ function FeaturesBar() {
           {FEATURES.map((item) => (
             <li
               key={item.text}
-              className="flex flex-col h-[60px] justify-center w-full cursor-pointer items-center max-w-[105px] sm:h-[74px] sm:justify-between"
+              className="flex flex-col h-[60px] justify-start w-full cursor-pointer items-center max-w-[105px] sm:h-[74px]"
             >
               <img
                 className="h-[27.87px] w-[35.63px] mb-[8px] sm:h-[42px] sm:mb-[12px] sm:w-[53.44px]"
                 src={item.img}
                 alt=""
               />
-              <p className="text-[#FFFFFF] text-[10px] text-center w-full font-body font-normal leading-[12px] sm:text-[14px] tracking-[0%] uppercase">
+              <p className="text-[#FFFFFF] text-[10px] text-center w-full font-body font-normal leading-[12px] sm:text-[14px] tracking-[0%] uppercase mb-auto">
                 {item.text}
               </p>
             </li>
